27a1fad84805e91187e390df109fd97c87672028
Eddy Barahona <barahona.hernandez2712@gmail.com>
2023-10-31 21:46:56 -0600

    :art: configurando la paleta de colores

--- clinica-uca-frontend/tailwind.config.js
+++ clinica-uca-frontend/tailwind.config.js
@@ -2,6 +2,19 @@
 module.exports = {
   content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      'primary': '#112A56',
+      'primary-dark': '#07142D',
+      'primary-light': '#173A76',
+      'secondary': '#9448BC',
+      'platinum': '#EAEAEA',
+      'error': '#B3261E',
+      'gray': '#9DA1A7',
+      'gray-dark': '#3C3F43',
+      'white': '#FFF',
+      'black': '#000',
+      transparent: 'transparent',
+    },
     extend: {},
   },
   plugins: [],
